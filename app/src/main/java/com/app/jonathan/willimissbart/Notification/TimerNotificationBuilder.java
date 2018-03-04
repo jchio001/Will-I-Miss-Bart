8b30aee0ef3178f6aac20646ebc155758708c642
Jonathan Chiou <jchio001@ucr.edu>
2018-03-03 17:16:14 -0800

    26 => 27 & removed jUnit import

--- app/src/main/java/com/app/jonathan/willimissbart/Notification/TimerNotificationBuilder.java
+++ app/src/main/java/com/app/jonathan/willimissbart/Notification/TimerNotificationBuilder.java
@@ -4,7 +4,7 @@ import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.NotificationCompat;
+import android.support.v4.app.NotificationCompat;
 import android.widget.RemoteViews;
 
 import com.app.jonathan.willimissbart.Misc.Constants;
--- app/src/test/java/com/app/jonathan/willimissbart/ExampleUnitTest.java
+++ app/src/test/java/com/app/jonathan/willimissbart/ExampleUnitTest.java
@@ -1,12 +1,4 @@
 package com.app.jonathan.willimissbart;
 
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
-    }
 }
