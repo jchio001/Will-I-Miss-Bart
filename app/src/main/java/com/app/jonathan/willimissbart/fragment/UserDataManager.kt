dbbd4b9559df2026fb9ef5fad9e20d2476212059
Jonathan Chiou <jchio001@ucr.edu>
2018-07-07 10:40:32 -0700

    Cleaned up UserDataManager constructor
    - In a Kotlin primary contructor, a parameter marked with val/var indicates a member variable

--- app/src/main/java/com/app/jonathan/willimissbart/fragment/UserDataManager.kt
+++ app/src/main/java/com/app/jonathan/willimissbart/fragment/UserDataManager.kt
@@ -10,13 +10,11 @@ import java.util.ArrayList
  * Manages user data (the stations they're travelling to and from and whether or not they want a
  * return route).
  */
-class UserDataManager {
+class UserDataManager(val spManager: SPManager, bundle: Bundle?) {
 
     interface UserDataSubscriber {
         fun onUserDataChanged(updatedUserData: ArrayList<UserStationData>, includeReturnRoute: Boolean)
     }
-
-    private val spManager : SPManager
 
     // A pair of station data. First one is origin
     private var userData : ArrayList<UserStationData>
@@ -25,8 +23,7 @@ class UserDataManager {
 
     private var includeReturnRoute : Boolean;
 
-    constructor(spManager: SPManager, bundle : Bundle?) {
-        this.spManager = spManager
+    init {
         userData = bundle?.getParcelableArrayList<UserStationData>(Constants.USER_DATA)
                 ?: spManager.fetchUserData()
         includeReturnRoute = spManager.fetchIncludeReturnRoute();
